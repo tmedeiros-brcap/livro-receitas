75913e63e2ba8f5c0208ccef694519753edc0ffd
Thiago Medeiros <medeirostalk@gmail.com>
2017-02-13 14:43:08 -0200

    Node Recipe

--- node.rb
+++ node.rb
@@ -1,3 +1,16 @@
-node.default['nodejs']['install_method'] = 'binary'  
-node.default['nodejs']['version'] = '5.10.0' 
+# resources/setup.rb
+property :nodejs_version, String, default: '5.10.1'  
+property :nodejs_checksum, String
 
+default_action :run
+
+action :run do  
+  # 1. install git
+  # include_recipe 'git'
+
+  # 2. install nodejs
+  node.default['nodejs']['install_method'] = 'binary'
+  node.default['nodejs']['version'] = nodejs_version
+  node.default['nodejs']['binary']['checksum']['linux_x64'] = nodejs_checksum
+  include_recipe 'nodejs'
+end  
